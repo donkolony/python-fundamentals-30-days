e1da970217b149bfb21076a7c78f11f26d064e53
donkolony <donkolony@gmail.com>
2025-12-09 14:32:00 +0200

    First commit, complete 01_Day_Introduction

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(f"(3 + 2) = {3 + 2}")  # addition(+)
+print(f"(3 - 2) = {3 - 2}")  # subtraction(-)
+print(f"(3 * 2) =  {3 * 2}")  # multiplication(*)
+print(f"(3 / 2) = {3 / 2}")  # division(/)
+print(f"(3 ** 2) =  {3 ** 2}")  # exponential(**)
+print(f"(3 % 2) =  {3 % 2}")  # modulus(%)
+print(f"(3 // 2) = {3 // 2}")  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))  # Int
+print(type(3.14))  # Float
+print(type(1 + 3j))  # Complex
+print(type("Don"))  # String
+print(type([1, 2, 3,]))  # List
+print(type({"name": "Don"}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))   # Set
+print(type((9.8, 3.14, 2, 7)))  # Tuple
+print(type(3 == 3))  # Bool
+print(type(3 >= 3))  # Bool
